8e44c0c237b297e6face998b9401b89702965f1c
Geoff Wilson <geoff@gr-oss.io>
2026-05-29 12:05:16 -0400

    Sqaush of branch
    Signed-off-by: Geoff Wilson <geoff@gr-oss.io>

--- core/schema/coremod.go
+++ core/schema/coremod.go
@@ -186,6 +186,7 @@ func (m *CoreMod) Install(ctx context.Context, dag *dagql.Server, _ ...core.Inst
 		&httpSchema{},
 		&platformSchema{},
 		&socketSchema{},
+		&volumeSchema{},
 		&moduleSourceSchema{},
 		&moduleSchema{},
 		&errorSchema{},
